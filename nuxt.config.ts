3a199239ff364dbb6882bfa6cf70945c5cb34084
Louie Chen <louieforcode@gmail.com>
2024-07-10 23:47:39 +0800

    build: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-07-10',
   ssr: false,
-  devtools: { enabled: false }
+  devtools: { enabled: false },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  },
+  css: ['~/assets/css/main.css']
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="text-3xl bg-stone-400">
         Index Page
     </div>
 </template>
